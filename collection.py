f93bbe61fd69f356c2bf99e177e3b5a6102884ac
Rithik076-ui <rithikstar076@gmail.com>
2025-08-04 13:23:27 +0530

    message

--- collection.py
+++ collection.py
@@ -1,40 +1,40 @@
-# collection : counter , nametuple,order dict,defaultdict,deque
-# from collections import Counter
-# a="aaaabbbccc"
-# Counter1 = Counter(a)
-# print(Counter1)
-# print(Counter1.items)
-# print(Counter1.keys())
-# print(Counter1.values())
+#collection : counter , nametuple,order dict,defaultdict,deque
+from collections import Counter
+a="aaaabbbccc"
+Counter1 = Counter(a)
+print(Counter1)
+print(Counter1.items)
+print(Counter1.keys())
+print(Counter1.values())
 
-# print(Counter1.most_common(1)[0][0])
-# # it can shows the element repeatation
-# print(list(Counter1.elements()))
-
-
-# from collections import namedtuple
-# point = namedtuple("point", "x,y")
-# pt = point(1,-4)
-# print(pt)
+print(Counter1.most_common(1)[0][0])
+# it can shows the element repeatation
+print(list(Counter1.elements()))
 
 
-
-
-# from collections import OrderedDict
-# Dict= OrderedDict()
-# Dict["b"] = 2
-# Dict["c"] = 3
-# Dict["d"] = 4
-# Dict["a"] = 1
-# print(Dict)
+from collections import namedtuple
+point = namedtuple("point", "x,y")
+pt = point(1,-4)
+print(pt)
 
 
 
-# from collections import defaultdict
-# k = defaultdict()
-# k['a'] = 1
-# k['b'] = 2
-# print(k['c'])
+
+from collections import OrderedDict
+Dict= OrderedDict()
+Dict["b"] = 2
+Dict["c"] = 3
+Dict["d"] = 4
+Dict["a"] = 1
+print(Dict)
+
+
+
+from collections import defaultdict
+k = defaultdict()
+k['a'] = 1
+k['b'] = 2
+print(k['c'])
 
 
 
