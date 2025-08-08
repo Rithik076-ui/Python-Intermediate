13f5ef2205f9b2287cf59adf64327adcc9876d77
Rithik076-ui <rithikstar076@gmail.com>
2025-08-08 22:52:20 +0530

    Today learning

---
+++ errors.py
@@ -0,0 +1,29 @@
+#Errors
+# a = 5 
+# print(a))
+
+# a = 5 + "10"
+
+# import somemodule
+
+
+# a = 5
+# b = c
+
+# f = open("somefile.txt")
+
+
+# a = [1,2,3]
+# a.remove(4)
+# print(a)
+
+
+# a = [1,2,3]
+# a[4]
+# print(a)
+
+
+# my_dict = {"name": 'max'}
+# my_dict['age']
+
+# a = 5 / 0
---
+++ exception.py
@@ -0,0 +1,67 @@
+# #Errors and Exception
+# x = 5
+# if x < 0:
+#     raise Exception('x sholud be positive')
+
+# x = 5
+# assert (x>=0), "x is not positive"
+
+# try:
+#     a = 5 / 0
+# except:
+#     print('an error happened')
+
+# try:
+#     a = 5 / 0
+# except Exception as e:
+#     print(e)
+
+# try:
+#     a = 5 / 0
+#     b = a + '10'
+# except ZeroDivisionError as r:
+#     print(r)
+# except TypeError as r :
+#     print(r)
+# else:
+#     print("everthing is fine")
+
+
+# try:
+#     a = 5 / 1
+#     b = a + 4
+# except ZeroDivisionError as r:
+#     print(r)
+# except TypeError as r :
+#     print(r)
+# else:
+#     print("everthing is fine")
+# finally:
+#     print("cleaning up...")
+
+
+
+class valueTooHighError(Exception):
+    pass
+
+class valuetoosmallerror(Exception):
+    def __init__(self, message, value):
+        self.message = message
+        self.value = value
+def test_value(x):
+    if x > 100:
+        raise valueTooHighError("value is too high:")
+    if x < 5:
+        raise valuetoosmallerror("value is too small:",x)
+    
+    
+try:
+    test_value(6)
+except valueTooHighError as r:
+    print(r)
+except valuetoosmallerror as r:
+    print(r.message, r.value)
+
+
+
+
